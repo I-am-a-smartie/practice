33c60e4c928647e18bf5f4a29d0d11c3b472d40b
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2020-04-02 12:09:54 +0530

    Lab1_1.cpp

---
+++ lab1_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int num1 = 0;
+    int num2 = 0;
+    int sum = 0;
+    cout << "Enter the first number- \n";
+    cin >> num1;
+    cout << "Enter the second number- \n";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum of your numbers is-" << sum << "\n";
+    return 0;
+}
