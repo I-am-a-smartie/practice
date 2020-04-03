a476650fd280b6d4b313a61a54e1c40f2256dcc1
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2020-04-03 09:51:19 +0530

    lab2_5.cpp

---
+++ lab2_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int base;
+    int index;
+    
+    cout << "Enter the base value\n";
+    cin >> base;
+    cout << "Enter the index value\n";
+    cin >> index;
+    cout << "The value of your base is-" << pow (base,index) << "\n";
+    
+}
