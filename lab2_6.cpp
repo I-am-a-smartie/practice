3aaf6a3a65a7d7f2edbf75679b614e66281176b4
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2020-04-03 09:58:53 +0530

    lab2_6.cpp

---
+++ lab2_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int base;
+    int root;
+    
+    cout << "Enter the base value\n";
+    cin >> base;
+    root = sqrt(base);
+    cout << "The value of your square root is-" << root << "\n";
+    
+}
